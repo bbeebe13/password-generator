e92cc2ccb2c3c1dc5bb739cb1f5f799166a20a00
Brody Beebe <bbeebe13@gmail.com>
2021-05-23 09:04:26 -0600

    password prompts added

--- script.js
+++ script.js
@@ -1,5 +1,34 @@
 // Assignment code here
+var lowerAlphabet = "abcdefghijklmnopqrstuvwxyz"
+var upperAlphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+var specChars = "!@#$%&*"
 
+var passwordLength = function() {
+  var passLength = window.prompt("Select password length. Enter a number between '8' and '128'.");
+  if (!passLength || passLength < 8 || passLength > 128) {
+    alert("Invalid entry. Please try again.");
+    passwordLength();
+  } else {
+    return passLength;
+  }
+}
+
+
+
+function generatePassword() {
+  //Prompt user for password length
+  passwordLength();
+  
+  //Prompt user for password criteria
+  var lowerCase = window.confirm("Would you like to include lower case letters in your password?");
+  var upperCase = window.confirm("Would you like to include upper case letters in your password?");
+  var numerics = window.confirm("Would you like to include numbers in your password?");
+  var specialChars = window.confirm("Would you like to include special characters in your password?");
+
+  for (var i = 0; i < passwordLength; i++) {
+
+  }
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
